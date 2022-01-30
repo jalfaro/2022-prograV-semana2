45b131c758aebb4212b5b505dfc2afe8d77c48bd
jalfaro <jalfaro.pineda@gmail.com>
2022-01-30 01:32:04 -0600

    Ejemplo Semana 2

---
+++ app/src/main/java/gt/edu/galileo/a2022_prograv_semana2/MainActivity.kt
@@ -0,0 +1,11 @@
+package gt.edu.galileo.a2022_prograv_semana2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout_example_layout)
+    }
+}
